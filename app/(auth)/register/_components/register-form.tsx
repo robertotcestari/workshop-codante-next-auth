0182b7f78721860f083c419080914316236382a4
Roberto Cestari <robertotcestari@gmail.com>
2024-03-15 09:52:11 -0300

    refactor register form

--- app/(auth)/register/_components/register-form.tsx
+++ app/(auth)/register/_components/register-form.tsx
@@ -1,34 +1,85 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { UserIcon } from '../_components/user-icon';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 export default function RegisterForm() {
   return (
-    <form className="text-left ">
-      <div className="space-y-6">
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="name">Nome</Label>
-          <Input name="name" type="name" id="name" placeholder="Fulano de Tal" />
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" type="email" id="email" placeholder="email@exemplo.com" />
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="password">Senha</Label>
-          <Input
-            name="password"
-            type="password"
-            id="password"
-            placeholder="********"
-          />
-        </div>
-      </div>
-      <Button size={'lg'} type="submit" className="w-full mt-10 ">
-        Login
-      </Button>
-    </form>
+    <>
+      <Card className="mx-auto max-w-96">
+        <CardHeader>
+          <CardTitle className="flex items-center justify-center gap-2">
+            <UserIcon className="w-6 h-6" />
+            Cadastre-se
+          </CardTitle>
+          <CardDescription>Crie uma conta gratuitamente</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="text-left ">
+            <div className="space-y-6">
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="name">Nome</Label>
+                <Input
+                  name="name"
+                  type="name"
+                  id="name"
+                  placeholder="Fulano de Tal"
+                />
+              </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  name="email"
+                  type="email"
+                  id="email"
+                  placeholder="email@exemplo.com"
+                />
+              </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="password">Senha</Label>
+                <Input
+                  name="password"
+                  type="password"
+                  id="password"
+                  placeholder="********"
+                />
+              </div>
+            </div>
+            <Button size={'lg'} type="submit" className="w-full mt-10 ">
+              Login
+            </Button>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Link
+            className={cn(
+              buttonVariants({ variant: 'link', size: 'sm' }),
+              'mt-2 mx-auto'
+            )}
+            href="/login"
+          >
+            Já possui conta?
+          </Link>
+        </CardFooter>
+      </Card>
+      <Link
+        className={cn(buttonVariants({ variant: 'link', size: 'lg' }), 'mt-8')}
+        href="/"
+      >
+        Voltar para Home
+      </Link>
+    </>
   );
 }
--- app/(auth)/register/layout.tsx
+++ app/(auth)/register/layout.tsx
@@ -2,16 +2,8 @@ import Link from 'next/link';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { redirect } from 'next/navigation';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { UserIcon } from './_components/user-icon';
 
 export default async function LoginPage({
   children,
@@ -20,33 +12,7 @@ export default async function LoginPage({
 }) {
   return (
     <main className="container p-20 mx-auto text-center">
-      <Card className="mx-auto max-w-96">
-        <CardHeader>
-          <CardTitle className='flex items-center justify-center gap-2'>
-            <UserIcon className='w-6 h-6' />
-            Cadastre-se
-          </CardTitle>
-          <CardDescription>Crie uma conta gratuitamente</CardDescription>
-        </CardHeader>
-        <CardContent>{children}</CardContent>
-        <CardFooter>
-          <Link
-            className={cn(
-              buttonVariants({ variant: 'link', size: 'sm' }),
-              'mt-2 mx-auto'
-            )}
-            href="/login"
-          >
-            Já possui conta?
-          </Link>
-        </CardFooter>
-      </Card>
-      <Link
-        className={cn(buttonVariants({ variant: 'link', size: 'lg' }), 'mt-8')}
-        href="/"
-      >
-        Voltar para Home
-      </Link>
+      {children}
     </main>
   );
 }
