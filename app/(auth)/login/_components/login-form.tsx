b7a5ba20cbf1b28aaecad5562416bb099ead18fa
Roberto Cestari <robertotcestari@gmail.com>
2024-03-18 09:34:38 -0300

    login refactor

--- app/(auth)/login/_components/login-form.tsx
+++ app/(auth)/login/_components/login-form.tsx
@@ -1,30 +1,60 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 export default function LoginForm() {
   return (
-    <form className="text-left ">
-      <div className="space-y-6">
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" type="email" id="email" placeholder="Email" />
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="password">Senha</Label>
-          <Input
-            name="password"
-            type="password"
-            id="password"
-            placeholder="password"
-          />
-        </div>
-      </div>
-      <Button size={'lg'} type="submit" className="w-full mt-10 ">
-        Login
-      </Button>
-    </form>
+    <Card className="mx-auto max-w-96">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Entre com email e senha</CardDescription>
+      </CardHeader>
+      <CardContent>
+        {' '}
+        <form className="text-left ">
+          <div className="space-y-6">
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input name="email" type="email" id="email" placeholder="Email" />
+            </div>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="password">Senha</Label>
+              <Input
+                name="password"
+                type="password"
+                id="password"
+                placeholder="password"
+              />
+            </div>
+          </div>
+          <Button size={'lg'} type="submit" className="w-full mt-10 ">
+            Login
+          </Button>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Link
+          className={cn(
+            buttonVariants({ variant: 'link', size: 'sm' }),
+            'mt-2 mx-auto'
+          )}
+          href="/register"
+        >
+          Não possui conta?
+        </Link>
+      </CardFooter>
+    </Card>
   );
 }
--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -1,15 +1,3 @@
-import { buttonVariants } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
-
 export default async function LoginLayout({
   children,
 }: {
@@ -17,30 +5,7 @@ export default async function LoginLayout({
 }) {
   return (
     <main className="container p-20 mx-auto text-center">
-      <Card className="mx-auto max-w-96">
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-          <CardDescription>Entre com email e senha</CardDescription>
-        </CardHeader>
-        <CardContent>{children}</CardContent>
-        <CardFooter>
-          <Link
-            className={cn(
-              buttonVariants({ variant: 'link', size: 'sm' }),
-              'mt-2 mx-auto'
-            )}
-            href="/register"
-          >
-            Não possui conta?
-          </Link>
-        </CardFooter>
-      </Card>
-      <Link
-        className={cn(buttonVariants({ variant: 'link', size: 'lg' }), 'mt-8')}
-        href="/"
-      >
-        Voltar para Home
-      </Link>
+      {children}
     </main>
   );
 }
